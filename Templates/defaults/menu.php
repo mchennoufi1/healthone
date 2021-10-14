112e177d169ce3f01da4390d8bf77bbf4e080ebe
mchennoufi1 <70711395+mchennoufi1@users.noreply.github.com>
2021-10-14 21:15:33 +0200

    nice

--- Templates/defaults/menu.php
+++ Templates/defaults/menu.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php">
-            Sportcenter
+        <img src="img/Logo-Health-One.png" width="100px" height="80px"></img>
         </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
--- public/categories.php
+++ public/categories.php
@@ -18,7 +18,7 @@ include_once ('../Templates/defaults/menu.php');
             <div class="col-sm-4 col-md-3">
                 <div class="card">
                     <div class="card-body text-center">
-                        <a href="categories/2">
+                        <a href="categories/1">
                             <img class="product-img img-responsive center-block" src='img/categories/roeitrainer.jpg'/>
                         </a>
                         <div class="card-title mb-3">Roeitrainer</div>
