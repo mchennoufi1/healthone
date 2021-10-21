0934d9049fb88e85632203a6354667086640bc59
mchennoufi1 <70711395+mchennoufi1@users.noreply.github.com>
2021-10-22 01:33:48 +0200

    nice

--- Templates/home.php
+++ Templates/home.php
@@ -1,12 +1,9 @@
 <!DOCTYPE html>
     <html>
-    <?php
-    include_once('defaults/head.php');
-    ?>
     <body>
         <div class="container">
             <?php
-            include_once ('defaults/header.php');
+            include_once('defaults/head.php');
             include_once ('defaults/menu.php');
             include_once ('defaults/pictures.php');
             ?>
@@ -15,11 +12,9 @@
             <p>
                 Fit en gezond zijn is geen vanzelfsprekendheid. We moeten er zelf wat voor doen. Goede, gezonde voeding is hiervoor de basis.
                 Bewegen hoort hier ook bij. Regelmatig bewegen zorgt voor een goede doorbloeding en draagt bij aan ontspanning van lichaam en geest.
-                Sporten is goed voor sterkere spieren en voor de conditie. Sporcenter HealthOne heeft verschillende sportapparaten om mee te kunnen werken aan je conditie.
+                Sporten is goed voor sterkere spieren en voor de conditie. Sportcenter HealthOne heeft verschillende sportapparaten om mee te kunnen werken aan je conditie.
             </p>
-            
             <hr>
-
             <?php
                 include_once ('defaults/footer.php');
             ?>
